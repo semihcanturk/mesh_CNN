6bf3e8138b281f3ea6b941e97766df14b88c70ae
Semih Cantürk <canturks@seas.upenn.edu>
2018-08-31 17:29:30 +0300

    mesh traversal client complete with icosahedron example

--- mesh_traversal.py
+++ mesh_traversal.py
@@ -15,66 +15,50 @@ mesh = om.TriMesh()
 
 # In[3]:
 from numpy import genfromtxt
-data0 = genfromtxt('data0.csv', delimiter=',')
-data1 = genfromtxt('data1.csv', delimiter=',')
-
-# In[4]:
-data1 = data1.astype(int)
-
-# In[5]:
-verts = []
-for i in data0:
-    verts.append(mesh.add_vertex(i))
-
-# In[6]:
-faces = []
-for i in data1:
-    faces.append(mesh.add_face(verts[i[0]], verts[i[1]], verts[i[2]]))
-
-# In[7]:
-mesh.points()
-
-# In[8]:
-om.write_mesh('mesh.off', mesh)
-
-# In[9]:
-om.read_trimesh('mesh.off')
-
-# In[10]:
-print(data1)
-
-# In[11]:
-print(data0.shape)
-
-# In[12]:
-adj_mtx = np.zeros(shape=(32492, 32492))
-
-# In[13]:
-for i in data1:
-    p1 = i[0]
-    p2 = i[1]
-    p3 = i[2]
-
-    adj_mtx[p1][p2] = 1
-    adj_mtx[p2][p1] = 1
-
-    adj_mtx[p1][p3] = 1
-    adj_mtx[p3][p1] = 1
-
-    adj_mtx[p2][p3] = 1
-    adj_mtx[p3][p2] = 1
 
 
 # In[15]
 # API
-def get_dist(v,j):
-    coords1 = data0[v]
-    coords2 = data0[j]
+def create_adj_mtx(coords_file, tri_file):
+    if isinstance(coords_file, str) and isinstance(tri_file, str):
+        coords = genfromtxt(coords_file, delimiter=',')  # these include the coordinates for each point
+        triangles = genfromtxt(tri_file, delimiter=',')  # these include the vertices in each triangle
+        triangles = triangles.astype(int)
+    else:
+        coords = coords_file
+        triangles = tri_file
+
+    verts = []
+    for i in coords:
+        verts.append(mesh.add_vertex(i))
+    faces = []
+    for i in triangles:
+        faces.append(mesh.add_face(verts[i[0]], verts[i[1]], verts[i[2]]))
+    adj_mtx = np.zeros(shape=(coords.shape[0], coords.shape[0]))
+    for i in triangles:
+        p1 = i[0]
+        p2 = i[1]
+        p3 = i[2]
+
+        adj_mtx[p1][p2] = 1
+        adj_mtx[p2][p1] = 1
+
+        adj_mtx[p1][p3] = 1
+        adj_mtx[p3][p1] = 1
+
+        adj_mtx[p2][p3] = 1
+        adj_mtx[p3][p2] = 1
+    return adj_mtx, coords, triangles
+
+
+def get_dist(coords, v,j):
+    coords1 = coords[v]
+    coords2 = coords[j]
     return math.sqrt((coords1[0]-coords2[0])**2 + (coords1[1]-coords2[1])**2 + (coords1[2]-coords2[2])**2)
 
 
 # In[16]
-def get_order(ix_list, closest_ix):
+def get_order(adj_mtx, coords, ix_list, closest_ix):
     arr = []
     seen = []
     arr.append(closest_ix)
@@ -95,12 +79,15 @@ def get_order(ix_list, closest_ix):
             neigh_list.append(ix_list[ct])
         ct += 1
 
+    if len(arr) == len(ix_list):
+        return arr
+
     while (len(arr) != len(ix_list)):
         if len(neigh_list) == 2:
             v1 = neigh_list[0]
             v2 = neigh_list[1]
-            x1 = data0[v1]
-            x2 = data0[v2]
+            x1 = coords[v1]
+            x2 = coords[v2]
             if x1[0] <= x2[0]:
                 v = v1
                 arr.append(v)
@@ -144,7 +131,7 @@ def get_order(ix_list, closest_ix):
 
 
 # In[17]:
-def find_region(vertex, r):
+def find_region(adj_mtx, coords, vertex, r):
     verts = []
 
     # level_0
@@ -157,7 +144,7 @@ def find_region(vertex, r):
     ix_list = ix_list[0]
     dists = []
     for j in ix_list:
-        d = get_dist(v, j)
+        d = get_dist(coords, v, j)
         dists.append(d)
     ix_min = ix_list[dists.index(min(dists))]
     closest_ix = ix_min
@@ -166,7 +153,7 @@ def find_region(vertex, r):
     for i in range(1, r + 1):
         # this is the closest vertex of the new level
         # find the ordering of the level
-        arr = get_order(ix_list, closest_ix)
+        arr = get_order(adj_mtx, coords, ix_list, closest_ix)
         verts = verts + arr
         # get next level: for each in ix_list, get neighbors that are not in <verts>, then add them to the new list
         next_list = []
@@ -181,11 +168,65 @@ def find_region(vertex, r):
         next_list = list(set(next_list))
 
         # find starting point of next level using line eq
-        c1 = data0[vertex]
-        c2 = data0[closest_ix]
+        c1 = coords[vertex]
+        c2 = coords[closest_ix]
         line_dists = []
         for j in next_list:
-            c3 = data0[j]
+            c3 = coords[j]
+            line_dist = LA.norm(np.cross(c2 - c1, c1 - c3)) / LA.norm(c2 - c1)  # not exactly sure of this
+            line_dists.append(line_dist)
+        ix_list = next_list
+        closest_ix = next_list[line_dists.index(min(line_dists))]
+    return verts
+
+
+# In[17]:
+def traverse_mesh(coords, triangles, center):
+    adj_mtx, coords, triangles = create_adj_mtx(coords, triangles)
+
+    vertex = center
+    verts = []
+
+    # level_0
+    verts.append(vertex)
+    v = vertex
+
+    # find closest point in level 1
+    row = adj_mtx[v]
+    ix_list = np.nonzero(row)
+    ix_list = ix_list[0]
+    dists = []
+    for j in ix_list:
+        d = get_dist(coords, v, j)
+        dists.append(d)
+    ix_min = ix_list[dists.index(min(dists))]
+    closest_ix = ix_min
+
+    # levels_>=1
+    while len(verts) != len(adj_mtx[0]):    # until all vertices are seen
+        # this is the closest vertex of the new level
+        # find the ordering of the level
+        arr = get_order(adj_mtx, coords, ix_list, closest_ix)
+        verts = verts + arr
+        # get next level: for each in ix_list, get neighbors that are not in <verts>, then add them to the new list
+        next_list = []
+        for j in ix_list:
+            new_row = adj_mtx[j]
+            new_row = np.nonzero(new_row)
+            new_row = new_row[0]
+
+            for k in new_row:
+                if k not in verts:
+                    next_list.append(k)
+        next_list = list(set(next_list))
+        if len(next_list) == 0:
+            continue
+        # find starting point of next level using line eq
+        c1 = coords[vertex]
+        c2 = coords[closest_ix]
+        line_dists = []
+        for j in next_list:
+            c3 = coords[j]
             line_dist = LA.norm(np.cross(c2 - c1, c1 - c3)) / LA.norm(c2 - c1)  # not exactly sure of this
             line_dists.append(line_dist)
         ix_list = next_list
@@ -194,21 +235,21 @@ def find_region(vertex, r):
 
 
 # In[18]:
-def mesh_strider(mesh, center, radius, max_radius = 3):
+def mesh_strider(mesh, adj_mtx, coords, center, radius, max_radius = 3):
     # max_radius: the radius covering the whole mesh
     patches = []
-    vertices = find_region(center, max_radius-1)   # list of vertices, ordered
+    vertices = find_region(adj_mtx, coords, center, max_radius-1)   # list of vertices, ordered
     for v in vertices:                             # -1 because edges cant be centers
-        patches.append(find_region(v, radius))     # so no patches with them as centers
+        patches.append(find_region(adj_mtx, coords, v, radius))     # so no patches with them as centers
     return patches
 
 
 # In[19]
-def mesh_convolve(mesh, filters):
+def mesh_convolve(mesh, filters, adj_mtx, coords):
     center = 93 # arbitrary
     r = 1   # arbitrary
     max_radius = 20 # this should be derived from the mesh itself
-    strided_mesh = mesh_strider(mesh, center, r, max_radius=max_radius)
+    strided_mesh = mesh_strider(mesh, adj_mtx, coords, center, r, max_radius=max_radius)
     arr = []
     for f in filters:
         row = []
--- mesh_traversal_client.py
+++ mesh_traversal_client.py
@@ -6,43 +6,62 @@ import mesh_traversal
 import numpy as npo
 import math
 from numpy import linalg as LA
+
+# neighbor_count = adj_mtx.sum(axis=0)    # vertices with ids 0:11 have 5 neighbors (causing 5-bug), all others 6
+# neighs_0 = npo.nonzero(adj_mtx[0])
+# print(neighs_0) # [12, 68, 1720, 3372, 11800]
+
+# here is an icosahedron, everyone's favorite platonic solid
+r = (1.0 + math.sqrt(5.0)) / 2.0
+coords = np.array([
+    [-1.0,   r, 0.0],
+    [ 1.0,   r, 0.0],
+    [-1.0,  -r, 0.0],
+    [ 1.0,  -r, 0.0],
+    [0.0, -1.0,   r],
+    [0.0,  1.0,   r],
+    [0.0, -1.0,  -r],
+    [0.0,  1.0,  -r],
+    [  r, 0.0, -1.0],
+    [  r, 0.0,  1.0],
+    [ -r, 0.0, -1.0],
+    [ -r, 0.0,  1.0],
+], dtype=float)
+
+faces = np.array([
+    [0, 11, 5],
+    [0, 5, 1],
+    [0, 1, 7],
+    [0, 7, 10],
+    [0, 10, 11],
+    [1, 5, 9],
+    [5, 11, 4],
+    [11, 10, 2],
+    [10, 7, 6],
+    [7, 1, 8],
+    [3, 9, 4],
+    [3, 4, 2],
+    [3, 2, 6],
+    [3, 6, 8],
+    [3, 8, 9],
+    [5, 4, 9],
+    [2, 4, 11],
+    [6, 2, 10],
+    [8, 6, 7],
+    [9, 8, 1],
+])
+
 mesh = om.TriMesh()
 
-from numpy import genfromtxt
-data0 = genfromtxt('data0.csv', delimiter=',')  # these include the coordinates for each point
-data1 = genfromtxt('data1.csv', delimiter=',')  # these include the vertices in each triangle
+vlist = []
+for i in coords:
+    vlist.append(mesh.add_vertex(i))
 
-data1 = data1.astype(int)
+flist = []
+for i in faces:
+    flist.append(mesh.add_face(vlist[i[0]], vlist[i[1]], vlist[i[2]]))
 
-verts = []
-for i in data0:
-    verts.append(mesh.add_vertex(i))
+om.write_mesh('icosahedron.off', mesh)
 
-faces = []
-for i in data1:
-    faces.append(mesh.add_face(verts[i[0]], verts[i[1]], verts[i[2]]))
-
-om.write_mesh('mesh.off', mesh)
-
-adj_mtx = np.zeros(shape=(32492, 32492))
-
-for i in data1:
-    p1 = i[0]
-    p2 = i[1]
-    p3 = i[2]
-
-    adj_mtx[p1][p2] = 1
-    adj_mtx[p2][p1] = 1
-
-    adj_mtx[p1][p3] = 1
-    adj_mtx[p3][p1] = 1
-
-    adj_mtx[p2][p3] = 1
-    adj_mtx[p3][p2] = 1
-
-neighbor_count = adj_mtx.sum(axis=0)    # vertices with ids 0:11 have 5 neighbors (causing 5-bug), all others 6
-neighs_0 = npo.nonzero(adj_mtx[0])
-print(neighs_0) # [12, 68, 1720, 3372, 11800]
-
-patch_5_bug = mesh_traversal.find_region(1720, 3)
-print(patch_5_bug)
+vertex_list = mesh_traversal.traverse_mesh(coords, faces, 0)
+print(vertex_list)
