099f106612660a74a008650c0d65328c699bbf5a
Semih Cantürk <canturks@seas.upenn.edu>
2018-09-04 19:21:37 +0300

    mesh_from_file example and HCP data added

---
+++ examples/mesh_from_file.py
@@ -0,0 +1,34 @@
+"""
+mesh_from_file.py
+Loads an HCP surface file and creates a mesh from it.
+"""
+_author_ = "Semih Cantürk"
+_credits_ = "Cassiano Becker"
+
+import os
+import nibabel as nib
+import openmesh as om
+
+file_gii = '../data/HCP/convdata/100206.L.very_inflated.32k_fs_LR.surf.gii'
+file_gii = os.path.join(file_gii)
+img = nib.load(file_gii)
+
+img.print_summary()
+
+# these are the spatial coordinates
+data0 = img.darrays[0].data
+
+# these are the mesh connections
+data1 = img.darrays[1].data
+
+mesh = om.TriMesh()
+
+verts = []
+for i in data0:
+    verts.append(mesh.add_vertex(i))
+
+faces = []
+for i in data1:
+    faces.append(mesh.add_face(verts[i[0]], verts[i[1]], verts[i[2]]))
+
+om.write_mesh('example_mesh.off', mesh)
